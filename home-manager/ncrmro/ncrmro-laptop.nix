2730f3c05db5c04cabec0a3f4cf36d958c5e9038
Nicholas Romero <ncrmro@gmail.com>
2025-07-27 12:55:13 -0500

    chore: use monitor descriptions

--- home-manager/ncrmro/ncrmro-laptop.nix
+++ home-manager/ncrmro/ncrmro-laptop.nix
@@ -15,8 +15,9 @@
     # Environment variables
     # https://wiki.hyprland.org/Configuring/Variables/#input
     monitor = [
-      "DP-7, 3840x2160@60.00Hz, 0x0, 1"
-      "eDP-1, 2256x1504@60.00Hz, 3840x500, 1"
+      "desc:LG Electronics LG Ultra HD 0x00044217, 3840x2160@60.00Hz, 0x0, 1"
+      "desc:BOE 0x0BCA, 2256x1504@60.00Hz, 3840x500, 1"
     ];
+    workspace = "1,monitor:desc:LG Electronics LG Ultra HD 0x00044217,default=true";
   };
 }
