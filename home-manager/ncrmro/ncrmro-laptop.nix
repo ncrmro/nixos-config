90514de3a99e6fb34ad429b1640b82f33341034e
Nicholas Romero <ncrmro@gmail.com>
2025-07-26 19:57:13 -0500

    chore: move second screen up a bit

--- home-manager/ncrmro/ncrmro-laptop.nix
+++ home-manager/ncrmro/ncrmro-laptop.nix
@@ -14,7 +14,7 @@
     # https://wiki.hyprland.org/Configuring/Variables/#input
     monitor = [
       "DP-7, 3840x2160@60.00Hz, 0x0, 1"
-      "eDP-1, 2256x1504@60.00Hz, 3840x2000, 1"
+      "eDP-1, 2256x1504@60.00Hz, 3840x500, 1"
     ];
   };
 }
