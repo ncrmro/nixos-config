d9d2b98adc091219dcf410bc2f38a8dc686f957b
Nicholas Romero <ncrmro@gmail.com>
2025-07-28 12:31:38 -0500

    feat: check in working monitor worspace mapping

--- home-manager/ncrmro/ncrmro-laptop.nix
+++ home-manager/ncrmro/ncrmro-laptop.nix
@@ -18,6 +18,26 @@
       "desc:LG Electronics LG Ultra HD 0x00044217, 3840x2160@60.00Hz, 0x0, 1"
       "desc:BOE 0x0BCA, 2256x1504@60.00Hz, 3840x500, 1"
     ];
-    workspace = "1,monitor:desc:LG Electronics LG Ultra HD 0x00044217,default=true";
+    workspace = [
+      "1, monitor:desc:LG Electronics LG Ultra HD 0x00044217, persistent:true"
+      "2, monitor:desc:LG Electronics LG Ultra HD 0x00044217, persistent:true"
+      "3, monitor:desc:LG Electronics LG Ultra HD 0x00044217, persistent:true"
+      "4, monitor:desc:LG Electronics LG Ultra HD 0x00044217, persistent:true"
+      "5, monitor:desc:LG Electronics LG Ultra HD 0x00044217, persistent:true"
+      "6, monitor:desc:BOE 0x0BCA, persistent:true, default:true"
+    ];
+  };
+  home.packages = with pkgs; [
+    google-chrome
+  ];
+  programs.git.extraConfig = {
+      credential.helper = "store";
+      push.autoSetupRemote = true;
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      user.signingkey = "~/.ssh/id_ed25519";
+      includeIf."gitdir:~/code/unsupervised/" = {
+        path = "~/code/unsupervised/.gitconfig";
+      };
   };
 }
