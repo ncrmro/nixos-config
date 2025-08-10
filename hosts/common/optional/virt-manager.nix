a48b95cf522d693a8e43e613755b7d72b28fca48
Nicholas Romero <ncrmro@gmail.com>
2025-08-10 12:17:37 -0500

    docs: libvirt config

--- hosts/common/optional/virt-manager.nix
+++ hosts/common/optional/virt-manager.nix
@@ -2,4 +2,8 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
   users.users.ncrmro.extraGroups = [ "libvirtd" ];
+  # This will need be set in the home-manager config
+  # home.file.".config/libvirt/libvirt.conf".text = ''
+  #   uri_default = "qemu:///system"
+  # '';
 }
