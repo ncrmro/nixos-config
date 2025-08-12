ba89755dba24e7f904a7fbb9d775fd1328a23a76
Nicholas Romero <ncrmro@gmail.com>
2025-08-12 16:39:41 -0500

    feat: virt

--- home-manager/common/features/virtualization.nix
+++ home-manager/common/features/virtualization.nix
@@ -2,4 +2,7 @@
   home.file.".config/libvirt/libvirt.conf".text = ''
     uri_default = "qemu:///system"
   '';
+  home.file.".config/libvirt/qemu.conf".text = ''
+    nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]
+  '';
 }
