15e078d2002370c05579a470d367f9377aa02d86
Nicholas Romero <ncrmro@gmail.com>
2025-09-01 14:41:50 -0500

    chore: clean up default.nix

--- hosts/ncrmro-laptop/default.nix
+++ hosts/ncrmro-laptop/default.nix
@@ -40,7 +40,6 @@
 
   users.users.ncrmro = {
     isNormalUser = true;
-    initialPassword = "changeme";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyrDBVcGK+pUZOTUA7MLoD5vYK/kaPF6TNNyoDmwNl2 ncrmro@ncrmro-laptop-fw7k"
     ];
