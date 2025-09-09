22133bd3f72b12fd5e0ed9d7335c04ad1ef66217
Nicholas Romero <ncrmro@gmail.com>
2025-09-09 11:58:31 -0500

    feat: add loki for monitoring

--- hosts/common/kubernetes/default.nix
+++ hosts/common/kubernetes/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./cert-manager.nix
     ./ingress-nginx.nix
+    ./loki.nix
     ./zfs-localpv.nix
     ./kube-prometheus-stack.nix
   ];
---
+++ hosts/common/kubernetes/loki.nix
@@ -0,0 +1,74 @@
+{...}: {
+  services.k3s.autoDeployCharts = {
+    loki = {
+      name = "loki";
+      repo = "https://grafana.github.io/helm-charts";
+      version = "6.39.0";
+      hash = "sha256-d91DPheDPX1HNydWhjgJjgPsyNlMBfbU+wLIJMLLvNE=";
+      targetNamespace = "monitoring";
+      createNamespace = false;
+      values = {
+        loki = {
+          schemaConfig = {
+            configs = [
+              {
+                from = "2024-04-01";
+                store = "tsdb";
+                object_store = "s3";
+                schema = "v13";
+                index = {
+                  prefix = "loki_index_";
+                  period = "24h";
+                };
+              }
+            ];
+          };
+          ingester = {
+            chunk_encoding = "snappy";
+          };
+          querier = {
+            max_concurrent = 4;
+          };
+          pattern_ingester = {
+            enabled = true;
+          };
+          limits_config = {
+            allow_structured_metadata = true;
+            volume_enabled = true;
+          };
+        };
+
+        deploymentMode = "SimpleScalable";
+
+        backend = {
+          replicas = 1;
+          persistence = {
+            storageClass = "ocean-nvme";
+          };
+        };
+        read = {
+          replicas = 1;
+        };
+        write = {
+          replicas = 1;
+          persistence = {
+            storageClass = "ocean-nvme";
+          };
+        };
+
+        minio = {
+          enabled = true;
+          persistence = {
+            storageClass = "ocean-nvme";
+          };
+        };
+
+        gateway = {
+          service = {
+            type = "ClusterIP";
+          };
+        };
+      };
+    };
+  };
+}
