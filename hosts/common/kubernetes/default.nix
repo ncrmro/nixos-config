6aa5b69b5eef7bf2142f1b9b23673ef00cfa65b9
Nicholas Romero <ncrmro@gmail.com>
2025-09-13 09:38:53 -0500

    feat: add postgres operator

--- hosts/common/kubernetes/default.nix
+++ hosts/common/kubernetes/default.nix
@@ -1,8 +1,10 @@
 {...}: {
   imports = [
     ./cert-manager.nix
+    ./cluster-issuer.nix
     ./ingress-nginx.nix
     ./loki.nix
+    ./pgo.nix
     ./zfs-localpv.nix
     ./kube-prometheus-stack.nix
   ];
---
+++ hosts/common/kubernetes/pgo.nix
@@ -0,0 +1,13 @@
+{...}: {
+  services.k3s.autoDeployCharts = {
+    cloudnative-pg = {
+      name = "cloudnative-pg";
+      repo = "https://cloudnative-pg.github.io/charts";
+      version = "0.26.0";
+      hash = "sha256-5Um2iHfHjWRaEITwTbrhV6nNhXeMdHbIegf8nEsTmOI=";
+      targetNamespace = "kube-system";
+      createNamespace = false;
+      values = {};
+    };
+  };
+}
