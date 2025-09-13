0509ea035af54224c3af8186e378b429172a859b
Nicholas Romero <ncrmro@gmail.com>
2025-09-13 09:37:21 -0500

    feat: ocean give static ip, route and dns

--- hosts/ocean/default.nix
+++ hosts/ocean/default.nix
@@ -11,6 +11,7 @@
     ../common/global
     ../common/optional/tailscale.nix
     ../common/optional/secureboot.nix
+    # ../common/optional/adguard-home.nix
     ./k3s.nix
     ./k3s-storage-classes.nix
     ../common/kubernetes/default.nix
@@ -25,12 +26,29 @@
   networking.hostId = "89cbac5f"; # generate with: head -c 8 /etc/machine-id
   networking.hostName = "ocean";
 
+  networking.interfaces.enp4s0 = {
+    ipv4.addresses = [
+      {
+        address = "192.168.1.10";
+        prefixLength = 24;
+      }
+    ];
+  };
+
+  networking.defaultGateway = {
+    address = "192.168.1.254";
+    interface = "enp4s0";
+  };
+
+  networking.nameservers = ["1.1.1.1" "8.8.8.8"];
+
   environment.systemPackages = [
     pkgs.sbctl
     pkgs.htop
     pkgs.usbutils
     pkgs.bottom
     pkgs.btop
+    pkgs.dig
   ];
 
   system.stateVersion = "25.11";
