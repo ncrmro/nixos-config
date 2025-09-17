615e9e21f6d03f02638fedcaf8ff14873857f2c0
Nicholas Romero <ncrmro@gmail.com>
2025-09-16 20:47:33 -0500

    chore: allow zfs to tolerate maia taints

--- hosts/common/kubernetes/zfs-localpv.nix
+++ hosts/common/kubernetes/zfs-localpv.nix
@@ -1,3 +1,7 @@
+# ZFS LocalPV CSI Driver
+# GitHub repo: https://github.com/openebs/zfs-localpv/tree/develop/deploy/helm/charts
+# Chart repo: https://openebs.github.io/zfs-localpv
+# Raw values: https://raw.githubusercontent.com/openebs/zfs-localpv/refs/heads/develop/deploy/helm/charts/values.yaml
 {...}: {
   services.k3s.autoDeployCharts = {
     zfs-localpv = {
@@ -8,6 +12,28 @@
       targetNamespace = "kube-system";
       createNamespace = false;
       values = {
+        # Configure tolerations to allow scheduling on maia node with custom taint
+        zfsController = {
+          tolerations = [
+            {
+              key = "ncrmro.com/region";
+              operator = "Equal";
+              value = "us-south-2";
+              effect = "NoSchedule";
+            }
+          ];
+        };
+
+        zfsNode = {
+          tolerations = [
+            {
+              key = "ncrmro.com/region";
+              operator = "Equal";
+              value = "us-south-2";
+              effect = "NoSchedule";
+            }
+          ];
+        };
       };
     };
   };
