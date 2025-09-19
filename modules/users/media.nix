cfae22ef70f2d498cc355e486b7694d9bfdea4b4
Nicholas Romero <ncrmro@gmail.com>
2025-09-19 13:58:24 -0500

    chore: set media group id

--- modules/users/media.nix
+++ modules/users/media.nix
@@ -1,6 +1,8 @@
 {...}: {
   # Create media user and group for NFS media sharing
-  users.groups.media = {};
+  users.groups.media = {
+    gid = 9999;
+  };
   users.users.media = {
     isSystemUser = true;
     group = "media";
