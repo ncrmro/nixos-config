cfb2a088dca14d2b5218a706d3fad94f8437f8e6
Nicholas Romero <ncrmro@gmail.com>
2025-10-07 06:43:05 -0500

    feat: add keystone

--- flake.nix
+++ flake.nix
@@ -37,6 +37,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    keystone = {
+      url = "github:ncrmro/keystone";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Secret management
     agenix = {
