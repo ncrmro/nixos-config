f6903c50065a1b0e11582c9586de06dca03f19cd
Nicholas Romero <ncrmro@gmail.com>
2025-11-29 13:42:57 -0600

    chore: add apple keys

--- modules/users/ncrmro.nix
+++ modules/users/ncrmro.nix
@@ -1,12 +1,11 @@
 {config, ...}: let
-  ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+  ifTheyExist = groups:
+    builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in {
   users.users.ncrmro = {
     isNormalUser = true;
     extraGroups =
-      [
-        "wheel"
-      ]
+      ["wheel"]
       ++ ifTheyExist [
         "media"
         "audio"
@@ -18,6 +17,8 @@ in {
       ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyrDBVcGK+pUZOTUA7MLoD5vYK/kaPF6TNNyoDmwNl2 ncrmro@ncrmro-laptop-fw7k"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAGBpgX+4rqqVdHNnLWFXPOyVMf3Cp00VbUCLyR6tP15qHWTO9OKyjRbHIxmwFfw2hkfzCKD9MtN8vheH2NWWzg= ncrmro@iphone-14-pro"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCUAyM7/owpfpJPuzQMmkmnlAcqB91QIfVsj1TueIU3hUtoHGR6FcKfFgJA5gkhww10A91M6iPSHD2kd/BNBGD4= ncrmro@ncrmro-laptop"
     ];
   };
 }
