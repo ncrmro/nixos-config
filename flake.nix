e88880f1d6ac8280c8489137e7e7c88b690677c3
Nicholas Romero <ncrmro@gmail.com>
2025-12-19 21:48:17 -0600

    chore

--- flake.nix
+++ flake.nix
@@ -90,166 +90,161 @@
     };
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      disko,
-      home-manager,
-      nixos-hardware,
-      nix-index-database,
-      agenix,
-      ...
-    }:
-    let
-      # Import custom overlays
-      overlays = import ./overlays { inherit inputs; };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    disko,
+    home-manager,
+    nixos-hardware,
+    nix-index-database,
+    agenix,
+    ...
+  }: let
+    # Import custom overlays
+    overlays = import ./overlays {inherit inputs;};
 
-      # Function to create system-specific packages with allowUnfree enabled
-      pkgsForSystem =
-        system:
-        import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-          };
-          overlays = overlays;
+    # Function to create system-specific packages with allowUnfree enabled
+    pkgsForSystem = system:
+      import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
         };
+        overlays = overlays;
+      };
 
-      # Same for unstable packages
-      unstablePkgsForSystem =
-        system:
-        import nixpkgs-unstable {
-          inherit system;
-          config = {
-            allowUnfree = true;
-          };
-          overlays = overlays;
+    # Same for unstable packages
+    unstablePkgsForSystem = system:
+      import nixpkgs-unstable {
+        inherit system;
+        config = {
+          allowUnfree = true;
         };
-    in
-    {
-      # Code formatter (official NixOS formatter)
-      formatter.x86_64-linux = (pkgsForSystem "x86_64-linux").nixfmt-rfc-style;
-      formatter.aarch64-darwin = (pkgsForSystem "aarch64-darwin").nixfmt-rfc-style;
+        overlays = overlays;
+      };
+  in {
+    # Code formatter (official NixOS formatter)
+    formatter.x86_64-linux = (pkgsForSystem "x86_64-linux").nixfmt-rfc-style;
+    formatter.aarch64-darwin = (pkgsForSystem "aarch64-darwin").nixfmt-rfc-style;
 
-      # Import NixOS and Home Manager modules
-      nixosModules = import ./modules/nixos;
-      homeManagerModules = import ./modules/home-manager;
+    # Import NixOS and Home Manager modules
+    nixosModules = import ./modules/nixos;
+    homeManagerModules = import ./modules/home-manager;
 
-      # NixOS system configurations
-      nixosConfigurations = {
-        # Desktop/workstation configuration
-        mox = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/mox ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-
-        # Home server configuration
-        maia = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/maia ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-
-        # Test VM configuration - Desktop testing VM
-        test-vm = nixpkgs.lib.nixosSystem {
-          modules = [
-            home-manager.nixosModules.default
-            {
-              boot.initrd.systemd.emergencyAccess = true;
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "backup";
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-                outputs = self;
-              };
-              home-manager.users.ncrmro = import ./home-manager/ncrmro/test-vm.nix;
-            }
-            ./hosts/test-vm
-          ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-
-        # Additional systems
-        testbox = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/testbox ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-
-        ncrmro-laptop = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/ncrmro-laptop ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-        devbox = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/devbox ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-        mercury = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/mercury ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-        catalystPrimary = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/catalystPrimary ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-        ocean = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/ocean ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
-        };
-        ncrmro-workstation = nixpkgs.lib.nixosSystem {
-          modules = [ ./hosts/workstation ];
-          specialArgs = {
-            inherit inputs self;
-            outputs = self;
-          };
+    # NixOS system configurations
+    nixosConfigurations = {
+      # Desktop/workstation configuration
+      mox = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/mox];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
         };
       };
 
-      # Home Manager configurations
-      homeConfigurations = {
-        "ncrmro@mox" = home-manager.lib.homeManagerConfiguration {
-          modules = [ ./home-manager/ncrmro/mox.nix ];
-          pkgs = pkgsForSystem "x86_64-linux";
-          extraSpecialArgs = { inherit inputs self; };
+      # Home server configuration
+      maia = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/maia];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
         };
-        "nicholas@unsup-macbook" = home-manager.lib.homeManagerConfiguration {
-          modules = [ ./home-manager/ncrmro/unsup-macbook.nix ];
-          pkgs = pkgsForSystem "aarch64-darwin";
-          extraSpecialArgs = { inherit inputs self; };
+      };
+
+      # Test VM configuration - Desktop testing VM
+      test-vm = nixpkgs.lib.nixosSystem {
+        modules = [
+          home-manager.nixosModules.default
+          {
+            boot.initrd.systemd.emergencyAccess = true;
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              outputs = self;
+            };
+            home-manager.users.ncrmro = import ./home-manager/ncrmro/test-vm.nix;
+          }
+          ./hosts/test-vm
+        ];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
         };
-        "ncrmro@ncrmro-macbook" = home-manager.lib.homeManagerConfiguration {
-          modules = [ ./home-manager/ncrmro/ncrmro-macbook.nix ];
-          pkgs = pkgsForSystem "aarch64-darwin";
-          extraSpecialArgs = { inherit inputs self; };
+      };
+
+      # Additional systems
+      # testbox = nixpkgs.lib.nixosSystem {
+      #   modules = [ ./hosts/testbox ];
+      #   specialArgs = {
+      #     inherit inputs self;
+      #     outputs = self;
+      #   };
+      # };
+
+      ncrmro-laptop = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/ncrmro-laptop];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
+        };
+      };
+      devbox = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/devbox];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
+        };
+      };
+      mercury = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/mercury];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
+        };
+      };
+      catalystPrimary = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/catalystPrimary];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
+        };
+      };
+      ocean = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/ocean];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
+        };
+      };
+      ncrmro-workstation = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/workstation];
+        specialArgs = {
+          inherit inputs self;
+          outputs = self;
         };
       };
     };
+
+    # Home Manager configurations
+    homeConfigurations = {
+      "ncrmro@mox" = home-manager.lib.homeManagerConfiguration {
+        modules = [./home-manager/ncrmro/mox.nix];
+        pkgs = pkgsForSystem "x86_64-linux";
+        extraSpecialArgs = {inherit inputs self;};
+      };
+      "nicholas@unsup-macbook" = home-manager.lib.homeManagerConfiguration {
+        modules = [./home-manager/ncrmro/unsup-macbook.nix];
+        pkgs = pkgsForSystem "aarch64-darwin";
+        extraSpecialArgs = {inherit inputs self;};
+      };
+      "ncrmro@ncrmro-macbook" = home-manager.lib.homeManagerConfiguration {
+        modules = [./home-manager/ncrmro/ncrmro-macbook.nix];
+        pkgs = pkgsForSystem "aarch64-darwin";
+        extraSpecialArgs = {inherit inputs self;};
+      };
+    };
+  };
 }
