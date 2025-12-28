b46c9418c1578cd84d0590f9360e3105886966cb
Nicholas Romero <ncrmro@gmail.com>
2025-12-28 00:09:44 -0600

    chore: add unsup air ssh config

--- home-manager/common/features/cli/ssh.nix
+++ home-manager/common/features/cli/ssh.nix
@@ -2,11 +2,17 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+{
   programs.ssh = {
     enable = true;
     matchBlocks = {
       "unsup-laptop.local" = {
+        setEnv = {
+          TERM = "xterm-256color";
+        };
+      };
+      "unsup-air.local" = {
         setEnv = {
           TERM = "xterm-256color";
         };
