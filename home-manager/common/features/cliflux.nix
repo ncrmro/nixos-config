d6827d81cf885dbb770f463333ca286c3b908075
Nicholas Romero <ncrmro@gmail.com>
2026-02-03 08:09:20 -0600

    feat: systemlink cliflux config

--- home-manager/common/features/cliflux.nix
+++ home-manager/common/features/cliflux.nix
@@ -1,4 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = [ pkgs.cliflux ];
+
+  # Symlink agenix-managed config (contains API key) at activation time
+  # Can't use xdg.configFile.source with /run paths in pure evaluation mode
+  home.activation.linkClifluxConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "$HOME/.config/cliflux"
+    ln -sf /run/agenix/cliflux-config "$HOME/.config/cliflux/config.toml"
+  '';
 }
